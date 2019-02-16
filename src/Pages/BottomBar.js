125e494b54d5fd021363d7c2b13d330ad5767316
Jiin  Rhew <jiinrhew@jiins-mbp-2.dyndns.rice.edu>
2019-02-16 13:28:13 -0600

    bottom grey bar page added

---
+++ src/Pages/BottomBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './BottomBar.css';
+
+export default class AboutUs extends Component {
+    render() {
+        return(
+          <div className="BottomBarWrapper">
+
+          </div>
+          )
+
+  }
+
+}
