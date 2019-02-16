28435c08f5c792c5fc52954bb7e26cfe5851dae2
Jiin  Rhew <jiinrhew@jiins-mbp-2.dyndns.rice.edu>
2019-02-16 14:49:10 -0600

    Bottom Bar created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import FrontPage from './Pages/FrontPage.js';
+import BottomBar from './Pages/BottomBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <FrontPage/>
+        <BottomBar/>
       </div>
     );
   }
--- src/Pages/BottomBar.js
+++ src/Pages/BottomBar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './BottomBar.css';
+import FB from '../Images/Facebook Icon.jpg';
+import Twitter from '../Images/Twitter Icon.jpg';
 
 export default class AboutUs extends Component {
     render() {
         return(
           <div className="BottomBarWrapper">
-
+            <div id="BBTextWrapper">
+              <div id="BBShortIntro">
+                <h1>Friends of Texas Wildlife</h1>
+                <p>Friends of Texas Wildlife is a 501(c)(3) non-profit organization;
+                100% of all donations support native Texas wildlife and are tax deductible
+                to the extent permitted by law.</p>
+              </div>
+              <div className="BBTitles">AboutUs</div>
+              <div className="BBTitles">Ways to Give</div>
+              <div className="BBTitles">Found an <br/>Animal</div>
+              <div className="BBTitles">News & <br/>Events</div>
+              <div className="BBTitles">Store</div>
+            </div>
+            <div className="SocialMIcons">
+              <img className="Icons" src = {FB}/>
+              <img className="Icons" src = {Twitter}/>
+            </div>
           </div>
           )
 
