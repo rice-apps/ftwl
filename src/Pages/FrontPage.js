f73b6c18a1b2ff622217b923813397ae833007c2
Victor Song <vsong1618@gmail.com>
2019-02-19 13:40:00 -0600

    started on menu bar

--- src/Pages/FrontPage.js
+++ src/Pages/FrontPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './FrontPage.css';
 
+import TopBar from './TopBar.js';
+
 export default class FrontPage extends Component {
     render() {
         return(
-            <div></div>
+            <div>
+                <TopBar/>
+            </div>
         )
     }
 }
