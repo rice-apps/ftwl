c65b70a6b43e660e12db365431093982ff7d15f8
Jiin  Rhew <jiinrhew@jiins-mbp-2.dyndns.rice.edu>
2019-02-23 13:59:26 -0600

    Ways To Give page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 import FrontPage from './Pages/FrontPage.js';
 import BottomBar from './Pages/BottomBar.js';
+import WaysToGive from './Pages/WaysToGive.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <FrontPage/>
-        <BottomBar/>
+        <WaysToGive/>
+        <BottomBar/>  
       </div>
     );
   }
