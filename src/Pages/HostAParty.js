5b021d6281e90f9151f8a0799722b3028ab21548
Jiin  Rhew <jiinrhew@jiins-mbp-2.dyndns.rice.edu>
2019-04-06 13:56:42 -0500

    Host a Party page updated

--- src/Pages/HostAParty.js
+++ src/Pages/HostAParty.js
@@ -6,7 +6,13 @@ export default class HostAParty extends Component {
         return(
           <div className="HostPartyWrapper">
             <h1>Host a Party</h1>
-            <div id="HostPartyWrapper">
+            <div id="HostPartyContentWrapper">
+              <div className="HostPartyTextWrapper">
+                <div className="HostPartyHeader">Give your child a wild party (lol this isn't done)</div>
+                <p>We are in need of many items to keep the animals happy and healthy. These are just some of the items we use on a daily basis. For specific items, see our Amazon wish list.</p>
+                <p>To donate items, visit our rehab center during our business hours or contact us.</p>
+              </div>
+              <div className="HostPartyImage"></div>
             </div>
           </div>
           )
